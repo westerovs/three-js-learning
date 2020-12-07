107d18d5841780dd7af527c09606ffd4c8de905d
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2020-12-07 18:53:20 +0300

    added 1.1 task

--- app/main.js
+++ app/main.js
@@ -1,27 +1,30 @@
 const links = document.querySelectorAll('.link')
 
 links.forEach((link, index) => {
-    if (index === 0) {
-        link.addEventListener('pointerdown', () => {
-            console.log(index)
-            createScript(1)
-        })
-    }
-
-    if (index === 1) {
-        link.addEventListener('pointerdown', () => {
-            console.log(index)
-            createScript(1.1)
-        })
+    switch (index) {
+        case 0:
+            link.addEventListener('pointerdown', () => {
+                console.log(index)
+                createScript(1)
+            })
+            break
+        case 1:
+            link.addEventListener('pointerdown', () => {
+                console.log(index)
+                createScript(1.1)
+            })
+            break
     }
 })
 
+// костыль
 function createScript(source) {
     const script = document.createElement('script')
     script.src = `tasks/${source}.js`
+
     document.querySelector('body').append(script)
 
-    setTimeout(() => {
-        script.remove()
-    }, 1000)
+    setTimeout(() => script.remove(), 100)
 }
+
+createScript(1.1)
--- app/tasks/1.1.js
+++ app/tasks/1.1.js
@@ -1,71 +1,66 @@
+const canvas = document.querySelector('#canvas')
+const renderer = new THREE.WebGLRenderer({canvas})
 
-    function main() {
-        const canvas = document.querySelector('#canvas');
-        const renderer = new THREE.WebGLRenderer({canvas});
+const fov         = 90  // поле зрения в градусах (field of view, поле зрения)
+const aspect      = 1   // соотношение сторон холста
+const near        = 0.1 // пространство перед камерой
+const far         = 5   // пространство перед камерой
+const camera      = new THREE.PerspectiveCamera(fov, aspect, near, far)
+camera.position.z = 2
 
-        const fov = 70;    // поле зрения в градусах (field of view, поле зрения)
-        const aspect = 1;  // соотношение сторон холста
-        const near = 0.1;  // пространство перед камерой
-        const far = 5;     // пространство перед камерой
+// Все, что вы хотите нарисовать необходимо добавить на сцену
+const scene = new THREE.Scene()
 
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.z = 2;
+// BoxGeometry который содержит данные для прямоугольного параллелепипеда.
+// Почти все, что мы хотим отобразить в Three.js, нуждается в геометрии,
+// которая определяет вершины нашего трехмерного объекта.
+const boxWidth  = 1
+const boxHeight = 1
+const boxDepth  = 1
+const geometry  = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
 
-        // Все, что вы хотите нарисовать необходимо добавить на сцену
-        const scene = new THREE.Scene();
+// Направленные источники имеет положение и цель. Оба по умолчанию равны 0, 0, 0
+const color = 0xFFFFFF
+const intensity = 2
+const light = new THREE.DirectionalLight(color, intensity)
+light.position.set(-1, 2, 4)
+scene.add(light)
 
-        // BoxGeometry который содержит данные для прямоугольного параллелепипеда.
-        // Почти все, что мы хотим отобразить в Three.js, нуждается в геометрии,
-        // которая определяет вершины нашего трехмерного объекта.
-        const boxWidth = 1;
-        const boxHeight = 1;
-        const boxDepth = 1;
-        const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+function createCube(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color: 0xFF7B1C})
 
-        // создать направленный свет
-        // Направленные источники имеет положение и цель. Оба по умолчанию равны 0, 0, 0
-        {
-            const color = 0xFFFFFF;
-            const intensity = 2;
-            const light = new THREE.DirectionalLight(color, intensity);
-            light.position.set(-1, 2, 4);
-            scene.add(light);
-        }
+    // создаем полигональную сетку Mesh.
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube) // добавляем Mesh на сцену
+    cube.position.x = x
 
+    return cube
+}
 
-        // создаем основной материал и устанавливаем его цвет
-        // const material = new THREE.MeshBasicMaterial({color: 0x770045});
+const cubes = [
+    createCube(geometry, 0x44aa88, 0),
+    createCube(geometry, 0x8844aa, -1.5),
+    createCube(geometry, 0xaa8844, 1.5),
+]
 
-        // меняем материал на блестящий
-        const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
+// отрендерить сцену, вызвав функцию render рендерера передав ей сцену и камеру.
+renderer.render(scene, camera)
 
-        /*
-          Затем мы создаем полигональную сетку Mesh.
-          Mesh в three.js представляет комбинацию формы объекта Geometry и Material
-          (как нарисовать объект, блестящий или плоский, какой цвет, какую текстуру(ры)
-          применить и т.д.) а также положение, ориентацию, и масштаб этого объекта в сцене.
-        */
-        const cube = new THREE.Mesh(geometry, material);
+function render(time) {
+    time *= 0.001  // конвертировать время в секунды
 
-        // добавляем Mesh на сцену
-        scene.add(cube);
+    cubes.forEach((cube, ndx) => {
+        const speed = 1 + ndx * 0.1
+        const rot = time * speed
+        cube.rotation.x = Math.sin(rot)
+        cube.rotation.y = Math.sin(rot)
+    })
 
-        // отрендерить сцену, вызвав функцию render рендерера передав ей сцену и камеру.
-        renderer.render(scene, camera);
+    renderer.render(scene, camera)
 
-        function render(time) {
-            time *= 0.0001;  // конвертировать время в секунды
+    requestAnimationFrame(render)
+}
 
-            cube.rotation.x = Math.sin(time) * 5 // вращение в радианах
-            cube.rotation.y = Math.sin(time) * 5 // вращение в радианах
+render()
 
-            renderer.render(scene, camera);
-
-            requestAnimationFrame(render);
-        }
-        render()
-
-    }
-
-    main();
 
