5f555abcdfae2a877fbe265b2d5ad1e0a2bb77c8
= <westerovs@mail.ru>
2021-06-08 15:47:44 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 const links = document.querySelectorAll('.link')
 
-links.forEach((link, index) => {
-    link.addEventListener('pointerdown', () => {
-        console.log(index)
-        createScript(index)
-    })
-})
-
-// костыль ! утечка памяти !
-function createScript(source) {
-    const script = document.createElement('script')
-
-    setTimeout(() => {
-        if (script.classList.contains('task-script')) {
-            script.remove()
-            return
-        }
-    }, 100)
-
-    script.setAttribute('type', 'module')
-    script.src = `js/tasks/${source}.js`
-    script.classList.add('task-script')
-
-    document.querySelector('body').append(script)
+const clearClass = (element, className = 'active') => {
+    element.forEach(item => item.classList.remove(className))
 }
 
-createScript(4)
+function init(startItem = 0) {
+    createScript(startItem)
+    
+    links.forEach((link, index) => {
+        if (startItem === index) link.classList.add('active')
+        
+        link.addEventListener('pointerdown', () => {
+            if (link.classList.contains('active')) return
+            
+            clearClass(links)
+            link.classList.add('active')
+            
+            createScript(index)
+        })
+    })
+}
+
+
+// костыль ! утечка памяти !
+function createScript(index) {
+    let script = document.createElement('script')
+    
+    script.setAttribute('type', 'module')
+    script.src = `js/tasks/${ index }.js`
+    script.classList.add('task-script')
+    
+    document.querySelector('body').append(script)
+    
+    // setTimeout(() => {
+    //     if (script.classList.contains('task-script')) {
+    //         script.remove()
+    //         script = null
+    //     }
+    // }, 100)
+}
+
+init()
